a04b261354ffdf390ea35d41f232610c98d02bb3
Michael Chadwick <michael.chadwick@gmail.com>
2015-10-05 00:41:50 -0700

    refaaaaaaactoooooor

--- guess.rb
+++ guess.rb
@@ -2,31 +2,35 @@
 # encoding: UTF-8
 
 class Guess
-  def initialize
-    @right_num = rand(100)+1
+  def initialize(top_num)
+    @range_top = top_num
+    @right_num = rand(0..@range_top)
     @guesses = 10
   end
-  
+
   def play
-    puts "Guess a number (in #{@guesses} guesses) from 1 to 100: "
+    puts "Guess a number (in #{@guesses} guesses) from 1 to #{@range_top}: "
     guesses_left = @guesses
+
     while guesses_left > 0 do
-      num = gets
-      num = num.to_i
+      num = STDIN.gets.chomp.to_i
+      guesses_left -= 1
+      guess_text = guesses_left == 1 ? 'guess' : 'guesses'
+
       case
       when num > @right_num
-        puts "Nope. Try a lower number."
+        puts "Nope. Try a lower number (#{guesses_left} #{guess_text} left)."
       when num < @right_num
-        puts "Nope. Try a higher number."
+        puts "Nope. Try a higher number (#{guesses_left} #{guess_text} left)."
       when num == @right_num
-        num_guesses = (@guesses-guesses_left)+1
-        puts "That's it! :D"
+        num_guesses = (@guesses-guesses_left)
+        puts 'You win! :D'
         puts "You guessed the number '#{@right_num}' in #{num_guesses} guesses!"
         return
       end
-      guesses_left -= 1
     end
-    puts "You lose :("
+
+    puts 'You lose! :\'('
     puts "The correct number was #{@right_num}."
   end
 end
