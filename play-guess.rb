6c41ecd0ff3ed2bdfe3a8b86d95aeb3036f90ce9
Michael Chadwick <michael.chadwick@gmail.com>
2016-03-04 11:10:05 -0800

    optional user-supplied RNG parameters enabled; refactored like whoa; option to keep playing game after win/lose

--- play-guess.rb
+++ play-guess.rb
@@ -3,12 +3,16 @@
 
 require_relative 'guess'
 
+RANGE_BOTTOM_DEFAULT = 1
+RANGE_TOP_DEFAULT = 100
+
 begin
-  arg = ARGV[0].to_i
-
-  top_num = arg > 0 ? arg : 100
-
-  Guess.new(top_num).play
+  # utilize player-specified values if valid
+  arg1 = (ARGV[0].nil? || ARGV[0].to_i == 0) ? RANGE_BOTTOM_DEFAULT : ARGV[0].to_i
+  arg2 = (ARGV[1].nil? || ARGV[1].to_i == 0) ? RANGE_TOP_DEFAULT : ARGV[1].to_i
+  ARGV.clear
+  new_game = Guess.new(arg1, arg2)
+  new_game.play
 rescue => e
   puts "Error: #{e}"
 end
